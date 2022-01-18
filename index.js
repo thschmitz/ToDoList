1470a3d1f71c005a5cbcbe089946f8a218e0e8f7
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2022-01-18 11:32:54 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const path = require("path")
-
+var bodyParser = require("body-parser")
 const app = express()
 
 app.engine("html", require("ejs").renderFile);
@@ -8,7 +8,17 @@ app.set("view engine", "html")
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
 var tarefas = ["Arrumar o quarto", "Comprar no supermercado"]
+
+app.post("/", (req, res) => {
+    tarefas.push(req.body.tarefa)
+    res.render("index", {tarefasList: tarefas})
+})
 
 app.get("/", (req,res) => {
 
